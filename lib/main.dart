0bf927b6eb679df67567b73638e46828b1810eaa
a-foriglio-dev <aforiglio@studenti.laziodigital.it>
2026-07-13 20:03:54 +0200

    Aggiornato main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'screen/journal_screen.dart'; // Rimosso lo slash iniziale per uniformità con le importazioni standard di Flutter
 
 void main() {
-  // 1. 🆕 Fissa l'inizializzazione dei binding nativi di Flutter.
+  // Fissa l'inizializzazione dei binding nativi di Flutter.
   // Essenziale quando si usano database locali (SQLite/SharedPreferences) all'avvio dell'app.
   WidgetsFlutterBinding.ensureInitialized();
   
@@ -24,7 +24,7 @@ class EchoApp extends StatelessWidget {
         fontFamily: 'Roboto',
         scaffoldBackgroundColor: const Color(0xff134e5e),
         
-        // 2. 🆕 Ottimizzazione per i FloatingActionButton
+        //  Ottimizzazione per i FloatingActionButton
         // Evita che i bottoni inferiori ereditino stili indesiderati o deformazioni del raggio
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
           elevation: 4,
